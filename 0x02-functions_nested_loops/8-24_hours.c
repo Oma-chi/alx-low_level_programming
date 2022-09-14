49185f14bdf27452e4e6c52b4a63723bbd3d25ef
okeke jacinta <jacintaokeke97@gmail.com>
2022-09-13 21:18:27 -0700

    task 5 - 11

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * jack_bauer - function that prints every minute of the day of jack bauer,
+ * dtarting from 00:00 t0 23:59, min loop counts min, while hour loop counts hours
+ * Return: 0
+ */
+void jack_bauer(void)
+{
+	int hours = 0;
+	int minutes = 0;
+	int hours_reminder;
+	int mins_reminder;
+
+	while (hours <= 23)
+	{
+		while (minutes <= 59)
+		{
+			mins_reminder = minutes % 10;
+			hours_reminder = hours % 10;
+			_putchar(hours / 10 + '0');
+			_putchar(hours_reminder + '0');
+			_putchar(':');
+			_putchar(minutes / 10 + '0');
+			_putchar(minutes_reminder + '0');
+			minutes++;
+			_putchar('\n');
+		}
+		hours++;
+		minutes = 0;
+	}
+}
+
