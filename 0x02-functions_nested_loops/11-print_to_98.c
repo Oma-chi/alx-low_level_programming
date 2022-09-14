28d4b93e067b85f21cd967290ef4bd8977d97b89
okeke jacinta <jacintaokeke97@gmail.com>
2022-09-13 22:05:18 -0700

    updates

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -3,7 +3,7 @@
 /**
  * print_to_98 - prints all natural numbers from imput to 98
  *
- * @n: int to be used 
+ * @n: int to be used
  * Return: 0
  */
 void print_to_98(int n)
@@ -11,7 +11,7 @@ void print_to_98(int n)
 	if (n >= 98)
 	{
 		while (n > 98)
-			printf("%d, ", n++);
+			printf("%d, ", n--);
 		printf("%d\n", n);
 	}
 	else
