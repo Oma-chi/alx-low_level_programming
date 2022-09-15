42b93f9ce55799572a9b2aa1240dd7236bd3d9e4
okeke jacinta <jacintaokeke97@gmail.com>
2022-09-14 21:27:20 -0700

    updates

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,7 +2,8 @@
 
 /**
  * print_time_table - prints the times table with parameter
- * @n: parameter
+ * 
+ * @n: num to be initialised
  * Return: returns nothing
  */
 void print_times_table(int n)
@@ -29,7 +30,7 @@ void print_times_table(int n)
 				if (result >= 100)
 				{
 					_putchar((result / 100) + '0');
-					_putchar((result / 10) +'0');
+					_putchar((result / 10) % 10 + '0');
 				}
 				else if (result <= 99 && result >= 10)
 				{
