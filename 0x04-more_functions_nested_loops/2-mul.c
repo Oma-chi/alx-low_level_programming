7e488ab04fef5c8ead8b11d30863b7a82a84cd70
okeke jacinta <jacintaokeke97@gmail.com>
2022-09-15 20:02:39 -0700

    more functions and loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two numbers
+ * @a: first number
+ * @b: second number
+ * return: returns result
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+#include "main.h"
+
+/**
+ * mul - multiplies two numbers
+ * @a: first number
+ * @b: second number
+ * return: returns result
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+#include "main.h"
+
+/**
+ * mul - multiplies two numbers
+ * @a: first number
+ * @b: second number
+ * return: returns result
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
